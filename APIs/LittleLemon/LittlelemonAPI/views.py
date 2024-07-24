7c87e9eb47c0a768c54aefff4bddf1bf51958141
andretokumoto <andretokumoto@gmail.com>
2024-07-24 15:14:35 -0300

    algumas configurações

--- APIs/LittleLemon/LittlelemonAPI/views.py
+++ APIs/LittleLemon/LittlelemonAPI/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+class RatingsView(View):
+    def get(self, request):
+        return JsonResponse({'message': 'Ratings view'})
