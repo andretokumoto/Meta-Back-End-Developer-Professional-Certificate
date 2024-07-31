b2c78c878aca3cf3497a1aa41387ed0c9169aa25
andretokumoto <andretokumoto@gmail.com>
2024-07-31 16:35:01 -0300

    class based views

--- APIs/Rest_framework/BookListAPI/urls.py
+++ APIs/Rest_framework/BookListAPI/urls.py
@@ -1,30 +1,10 @@
 from django.urls import path
 from . import views
 
-'''
+
 urlpatterns = [
-    path('books/', views.books)
+    #path('books/', views.books)
+    path('books/',views.BookList.as_view()),
+    path('books/<int:pk>',views.Book.as_view())
 ]
 
-'''
-urlpatterns = [
-    path('orders/', views.Orders.listOrders)
-]
-'''
-urlpatterns = [
-    path('books/', views.BookView.as_view(
-        {
-            'get': 'list',
-            'post': 'create',
-        }
-    )),
-    path('books/<int:pk>', views.BookView.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update',
-            'patch': 'partial_update',
-            'delete': 'destroy',
-        }
-    ))
-]
-'''
--- APIs/Rest_framework/BookListAPI/views.py
+++ APIs/Rest_framework/BookListAPI/views.py
@@ -2,37 +2,25 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 # from django.http import HttpResponse
 
-class Orders():
-    @staticmethod
-    @api_view(['GET'])
-    def listOrders(request):
-        return Response({'message': 'list of orders'}, status=status.HTTP_200_OK)
+@api_view(['GET','POST'])
+def books(request):
+    return Response('LIST OF BOOKS',status=status.HTTP_200_OK)
 
-class Books():
-    @staticmethod
-    @api_view(['GET','POST'])
-    def lisBooks(request):
-        return Response('list of the books',
-                    status=status.HTTP_200_OK)
-'''
-class BookView(viewsets.ViewSet):
-    def list(self, request):
-        return Response({"message": "All books"}, status=status.HTTP_200_OK)
+class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get('author')
+        if(author):
+            return Response({'message':'List of the books by '+ author},status.HTTP_200_OK)
+        return Response({'message':'List of the books'},status.HTTP_200_OK)
+    def post(self, request):
+        return Response({'Title':request.data.get('title')}, status.HTTP_201_CREATED)
 
-    def create(self, request):
-        return Response({"message": "Creating a book"}, status=status.HTTP_201_CREATED)
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({'message':'singue book with id ' + str(pk)}, status.HTTP_200_OK)
 
-    def update(self, request, pk=None):
-        return Response({"message": "Updating a book"}, status=status.HTTP_200_OK)
-
-    def retrieve(self, request, pk=None):
-        return Response({"message": "Displaying a book"}, status=status.HTTP_200_OK)
-
-    def partial_update(self, request, pk=None):
-        return Response({"message": "Partially updating a book"}, status=status.HTTP_200_OK)
-
-    def destroy(self, request, pk=None):
-        return Response({"message": "Deleting a book"}, status=status.HTTP_200_OK)
-'''
+    def put(self, request, pk):
+        return Response({"title":request.data.get('title')},status.HTTP_200_OK)
