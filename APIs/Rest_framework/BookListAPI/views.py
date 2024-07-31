c29ba59ff558c4cd963474ee839d0f9d13a42e85
andretokumoto <andretokumoto@gmail.com>
2024-07-31 14:20:07 -0300

    API View Decorator

--- APIs/Rest_framework/BookListAPI/views.py
+++ APIs/Rest_framework/BookListAPI/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 @api_view(['GET','POST'])
 def books(request):
-    return HttpResponse('list of the books',
+    return Response('list of the books',
                     status=status.HTTP_200_OK)
