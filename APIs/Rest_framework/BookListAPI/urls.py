3f4d6cf99713eabc8ad5b2ee40a4c389f9323d4a
andretokumoto <andretokumoto@gmail.com>
2024-07-31 15:12:55 -0300

    Routing to a class method

--- APIs/Rest_framework/BookListAPI/urls.py
+++ APIs/Rest_framework/BookListAPI/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
-
+'''
 urlpatterns = [
     path('books/',views.books)
+]
+'''
+urlpatterns = [
+	path('orders', views.Orders.listOrders)
 ]
--- APIs/Rest_framework/BookListAPI/views.py
+++ APIs/Rest_framework/BookListAPI/views.py
@@ -4,7 +4,15 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 #from django.http import HttpResponse
 
+
+'''
 @api_view(['GET','POST'])
 def books(request):
     return Response('list of the books',
                     status=status.HTTP_200_OK)
+'''
+class Orders():
+	@staticmethod
+	@api_view()
+	def listOrders(request):
+	    return Response({'message':'list of orders'}, 200)
