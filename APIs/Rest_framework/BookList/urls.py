94087181ca43435a0f9e71022bea8b570d3482c9
andretokumoto <andretokumoto@gmail.com>
2024-07-31 16:50:23 -0300

    Django debug toolbar

--- APIs/Rest_framework/BookList/urls.py
+++ APIs/Rest_framework/BookList/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('BookListAPI.urls'))
+    path('api/',include('BookListAPI.urls')),
+    path('_debug_/',include('debug_toolbar.urls'))
 ]
