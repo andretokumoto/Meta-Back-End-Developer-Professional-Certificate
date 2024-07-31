15b4e82ffda46a9605d777083cd7ec85435a954b
andretokumoto <andretokumoto@gmail.com>
2024-07-31 15:27:15 -0300

    ViewSets

--- APIs/Rest_framework/BookListAPI/urls.py
+++ APIs/Rest_framework/BookListAPI/urls.py
@@ -3,9 +3,29 @@ from . import views
 
 '''
 urlpatterns = [
-    path('books/',views.books)
+    path('books/', views.books)
 ]
 '''
+
+'''
 urlpatterns = [
-	path('orders', views.Orders.listOrders)
+    path('orders', views.Orders.listOrders)
+]
+'''
+
+urlpatterns = [
+    path('books/', views.BookView.as_view(
+        {
+            'get': 'list',
+            'post': 'create',
+        }
+    )),
+    path('books/<int:pk>', views.BookView.as_view(
+        {
+            'get': 'retrieve',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy',
+        }
+    ))
 ]
--- APIs/Rest_framework/BookListAPI/views.py
+++ APIs/Rest_framework/BookListAPI/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
-#from django.http import HttpResponse
+# from django.http import HttpResponse
 
-
-'''
-@api_view(['GET','POST'])
-def books(request):
-    return Response('list of the books',
-                    status=status.HTTP_200_OK)
-'''
 class Orders():
-	@staticmethod
-	@api_view()
-	def listOrders(request):
-	    return Response({'message':'list of orders'}, 200)
+    @staticmethod
+    @api_view(['GET'])
+    def listOrders(request):
+        return Response({'message': 'list of orders'}, status=status.HTTP_200_OK)
+
+class BookView(viewsets.ViewSet):
+    def list(self, request):
+        return Response({"message": "All books"}, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        return Response({"message": "Creating a book"}, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        return Response({"message": "Updating a book"}, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        return Response({"message": "Displaying a book"}, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, pk=None):
+        return Response({"message": "Partially updating a book"}, status=status.HTTP_200_OK)
+
+    def destroy(self, request, pk=None):
+        return Response({"message": "Deleting a book"}, status=status.HTTP_200_OK)
