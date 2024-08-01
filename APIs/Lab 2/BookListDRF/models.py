018d7af3154a2f409909020aae784ac8840cd39a
andretokumoto <andretokumoto@gmail.com>
2024-08-01 15:59:25 -0300

    Practical laboratory module 2

---
+++ APIs/Lab 2/BookListDRF/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
