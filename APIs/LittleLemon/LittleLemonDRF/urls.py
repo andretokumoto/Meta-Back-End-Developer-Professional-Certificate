5fca3b9f329fc2a357059d264620009b45b61f24
andretokumoto <andretokumoto@gmail.com>
2024-08-06 14:57:17 -0300

    Filtering and searching

--- APIs/LittleLemon/LittleLemonDRF/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-     path('menu-items', views.MenuItemsView.as_view()),
-     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
-]
--- APIs/LittleLemon/LittlelemonAPI/views.py
+++ APIs/LittleLemon/LittlelemonAPI/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 #from django.views import View
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from .models import Menuitem
 from .serializers import MenuItemSerializar
 
@@ -16,9 +16,21 @@ class RatingsView(View):
 @api_view(['GET','POST'])
 def menu_itens(request):
     if request.method == 'GET':
-        itens = Menuitem.objects.all()
+        itens = Menuitem.objects.select_related('category').all()
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        
+        if category_name:
+            itens = itens.filter(category__title=category_name)
+        if to_price:
+             itens = itens.filter(price__lte=to_price)
+        if search:
+             itens = itens.filter(title__startswich=search)
+        
         serialized_item = MenuItemSerializar(itens, many = True)
         return Response(serialized_item.data) 
+    
     if request.method == 'POST':
         serialized_item = MenuItemSerializar(data=request.data)
         serialized_item.is_valid(raise_exception=True)
