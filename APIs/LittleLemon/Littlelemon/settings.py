21cce1af231d550443e7284cf9bd889382791f26
andretokumoto <andretokumoto@gmail.com>
2024-08-07 16:04:23 -0300

    Registration and authentication endpoints with JWT

--- APIs/LittleLemon/Littlelemon/settings.py
+++ APIs/LittleLemon/Littlelemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'rest_framework_simplejwt',
     'LittlelemonAPI',
 ]
 
@@ -133,6 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework_xml.renderers.XMLRenderer', 
     ],
     'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         
@@ -146,4 +149,8 @@ REST_FRAMEWORK = {
 
 DJOSER = {
     "USER_ID_FIELD":"username",
+}
+
+SiMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
 }
