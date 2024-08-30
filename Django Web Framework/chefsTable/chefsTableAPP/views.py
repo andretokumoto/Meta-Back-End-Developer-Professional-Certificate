2588804577b8c1ed93042dd1c2c2128ae057b817
andretokumoto <andretokumoto@gmail.com>
2024-08-30 14:37:19 -0300

    mapping urls with params

--- Django Web Framework/chefsTable/chefsTableAPP/views.py
+++ Django Web Framework/chefsTable/chefsTableAPP/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('Heloo to my APP')
+
+def menuitem(request,dish):
+    items = {
+        'pasta':'is pasta',
+        'cook':'good cook',
+        'meat':'delicious one'
+    }
+    
+    description = items[dish]
+    
+    return HttpResponse(f"<h2> {dish} </h2>"+ description)
