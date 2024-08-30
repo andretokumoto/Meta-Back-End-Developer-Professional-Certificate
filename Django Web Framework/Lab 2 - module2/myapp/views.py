6129a51175f414db2a8cdcd8249c7757c807d356
andretokumoto <andretokumoto@gmail.com>
2024-08-30 15:18:46 -0300

    lab 2

---
+++ Django Web Framework/Lab 2 - module2/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def drinks(request,drink_name):
+
+    drink = {
+        'mocha':'type of coffee',
+        'tea': 'type of beverage',
+        'lemonade': 'type of refreshment',
+    }
+    
+    choice_of_drink = drink[drink_name]
+
+    return HttpResponse(f"<h2> {drink_name} </h2>"+choice_of_drink)
