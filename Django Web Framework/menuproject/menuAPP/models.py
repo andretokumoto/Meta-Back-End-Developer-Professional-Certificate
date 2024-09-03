24d28eb1a285c14ff659d26d02e07f5db223f1e5
andretokumoto <andretokumoto@gmail.com>
2024-09-03 12:43:42 -0300

    Working with Migrations

--- Django Web Framework/menuproject/menuAPP/models.py
+++ Django Web Framework/menuproject/menuAPP/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Menuitens(models.Model):
+    item_name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    year = models.IntegerField()
