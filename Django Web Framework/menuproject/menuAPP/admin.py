f83a795f424530fdb57444f3e32a00e3a81ba597
andretokumoto <andretokumoto@gmail.com>
2024-09-03 13:47:45 -0300

    Foreign Keys

--- Django Web Framework/menuproject/menuAPP/admin.py
+++ Django Web Framework/menuproject/menuAPP/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Menu, Menu_category
 
-# Register your models here.
+
+admin.site.register(Menu)
+admin.site.register(Menu_category)
--- Django Web Framework/menuproject/menuAPP/models.py
+++ Django Web Framework/menuproject/menuAPP/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-class Menuitens(models.Model):
+class Menu_category(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+    
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.FloatField(null=False)
+    category_id = models.ForeignKey(Menu_category, on_delete=models.PROTECT, default=None, related_name="category_name")
+    
+'''class Menuitens(models.Model):
     item_name = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
-    year = models.IntegerField()
+    year = models.IntegerField()'''
