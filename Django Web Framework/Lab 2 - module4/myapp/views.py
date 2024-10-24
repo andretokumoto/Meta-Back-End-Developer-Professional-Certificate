e1a601d42dd6d65b8d8f57a51f8841bc13812d27
andretokumoto <andretokumoto@gmail.com>
2024-10-24 12:43:03 -0300

    Lab working with templates

---
+++ Django Web Framework/Lab 2 - module4/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def book(request):
+    return render(request, 'book.html')
+
