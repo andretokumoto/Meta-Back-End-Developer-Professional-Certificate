ee1d6c474fda3b402f6482d70e83415443846a48
andretokumoto <andretokumoto@gmail.com>
2024-11-12 12:13:01 -0300

    string

---
+++ Programming in Python/Module 1/string.py
@@ -0,0 +1,16 @@
+name = 'Andre'
+print(name[2])
+
+a = 'string separada'\
+    ' por quebra de linha'
+    
+print(a)
+
+def say_hello():
+    return "Hello there!"
+
+# With parameters
+def say_hello(you):
+    return "Hello " +  you
+
+print(say_hello('andre'))
