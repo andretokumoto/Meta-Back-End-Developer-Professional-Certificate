2de43662ca112a98a72e21ed2a2072c0880241b9
andretokumoto <andretokumoto@gmail.com>
2024-11-14 13:01:01 -0300

    sets

---
+++ Programming in Python/Module 2/set.py
@@ -0,0 +1,13 @@
+set_a = {12,23,45,67}
+print(set_a)
+print()
+set_a.remove(45)
+print(set_a)
+print()
+set_b = {12,23,67,13,00}
+print(set_a.union(set_b))
+print()
+print(set_a.intersection(set_b))
+print()
+print(set_a.symmetric_difference(set_b))
+print()
