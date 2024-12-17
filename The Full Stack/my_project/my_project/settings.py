3e3a7c0b210f34f4fe437d7ed87b3b6a0d72ede8
andretokumoto <andretokumoto@gmail.com>
2024-12-17 16:22:41 -0300

    Configuring Django to connect to MySQL

--- The Full Stack/my_project/my_project/settings.py
+++ The Full Stack/my_project/my_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'root',
+        'PORT':'3306'
     }
 }
 
