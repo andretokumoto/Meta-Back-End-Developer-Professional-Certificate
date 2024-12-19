7e90c94856f9f2b43515bfa76703c6517b496126
andretokumoto <andretokumoto@gmail.com>
2024-12-19 13:16:15 -0300

    ORDER BY

--- Introduction to Databases for Back-End Development/module3.sql
+++ Introduction to Databases for Back-End Development/module3.sql
@@ -10,6 +10,13 @@ CREATE TABLE employees (
 
 insert into employees values(1,'james',2000);
 insert into employees values(2,'Carmen',4000);
+insert into employees values(2,'Charles',1000);
 
 select salary + 500 from employees;
 select salary + 500 from employees where salary < 3000;
+
+SELECT * FROM employees
+ORDER BY salary;
+
+SELECT * FROM employees
+ORDER BY salary DESC;
