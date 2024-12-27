4c55339c721109a0c6f1933ceb563da322ef23c4
andretokumoto <andretokumoto@gmail.com>
2024-12-27 12:48:37 -0300

    hash table

---
+++ Coding Interview Preparation/hash.py
@@ -0,0 +1,25 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [None] * size
+
+    def _hash_function(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        index = self._hash_function(key)
+        if self.table[index] is None:
+            self.table[index] = [(key, value)]
+        else:
+            self.table[index].append((key, value))
+
+    def get(self, key):
+        index = self._hash_function(key)
+        if self.table[index] is None:
+            return None
+        for k, v in self.table[index]:
+            if k == key:
+                return v
+        return None
+
+hash_table = HashTable(10)
